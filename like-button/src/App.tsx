822e87c6fd9238cda483168f5315a9e7c57e9472
nakaty <78027524+nakaty2023@users.noreply.github.com>
2024-11-01 21:52:00 +0900

    ボタンを作る場所を用意する

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TypeScriptはいいぞ</header>
+      <header className="App-header">
+        <LikeButton />
+      </header>
     </div>
   );
 }
-
+ 
+function LikeButton() {
+  return <span>いいねボタン予定地</span>;
+}
+ 
 export default App;
